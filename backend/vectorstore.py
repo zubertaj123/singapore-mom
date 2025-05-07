e6cd8a8d9f406590537179d5e14e4c726166619d
Zuber Taj <117841586+zubertaj123@users.noreply.github.com>
2025-05-07 18:11:43 -0400

    fix1

--- backend/vectorstore.py
+++
@@ -1,9 +0,0 @@
-import os
-from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
-
-INDEX_PATH = "faiss_index"
-
-def load_vectorstore():
-    embeddings = OpenAIEmbeddings()
-    return FAISS.load_local(INDEX_PATH, embeddings, allow_dangerous_deserialization=True)
